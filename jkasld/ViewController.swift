f862b8161981d9ef8de4b02ecb0af342433bb05a
Wes Anderson <wanderson@loaner-cc-original.local>
2016-07-26 14:09:08 -0500

    Initial Commit

---
+++ jkasld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jkasld
+//
+//  Created by Wes Anderson on 7/26/16.
+//  Copyright © 2016 Wes Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
